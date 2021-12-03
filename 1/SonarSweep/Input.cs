4f6bc50ebc4df5135b4e4f0626dd30a6f2831846
dgoncalves <dgoncalves@freewheel.com>
2021-12-03 12:20:32 +0000

    added first exercise

---
+++ 1/SonarSweep/Input.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SonarSweep
+{
+    public class Input
+    {
+        public static async Task<int[]> GetReadings()
+        {
+            try
+            {
+                using var sr = new StreamReader("input.txt");
+                var input = await sr.ReadToEndAsync();
+                
+                var depthReadings = input.Split(Environment.NewLine)
+                    .Where(s => !string.IsNullOrEmpty(s))
+                    .Select(s => int.Parse(s))
+                    .ToArray();
+                return depthReadings;
+            }
+            catch (FileNotFoundException ex)
+            {
+                throw new Exception("Failed to read the file", ex);
+            }
+        }
+    }
+}
---
+++ 1/SonarSweep/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SonarSweep
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine(CountDepthIncreases(await Input.GetReadings()));
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        private static int CountDepthIncreases(int[] depthReadings)
+        {
+            var countIncreases = 0;
+            var _ = depthReadings.Aggregate(int.MaxValue, (prev, next) =>
+            {
+                if (next > prev) countIncreases++;
+                return next;
+            });
+            return countIncreases;
+        }
+    }
+}
