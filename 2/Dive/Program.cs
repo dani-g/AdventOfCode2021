f9521f02b9f099f3c606a0b06855fe90e8124ef8
dgoncalves <dgoncalves@freewheel.com>
2021-12-05 17:46:44 +0000

    2nd day

---
+++ 2/Dive/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Dive
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var readings = await Input.GetReadings("input_2");
+            var (horizontal, depth) =  CalculateFinalPosition(readings);
+            Console.WriteLine($"({horizontal}, {depth})");
+            Console.WriteLine(horizontal * depth);
+            Console.Read();
+        }
+
+        private static (int Horizontal, int Depth) CalculateFinalPosition(string[] commands)
+        {
+            var submarine = new Submarine();
+
+            foreach (var command in commands)
+            {
+                var splitCommand = command.Split(" ");
+                submarine.MoveWithAim(splitCommand[0], int.Parse(splitCommand[1]));
+            }
+            return (submarine.Horizontal, submarine.Depth);
+        }
+    }
+}
