1fbf43ae02bf9ac4be85360a078fb024f67b25a0
dgoncalves <dgoncalves@freewheel.com>
2021-12-07 15:18:41 +0000

    started day 3

---
+++ 3/BinaryDiagnostics/Input.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryDiagnostics
+{
+    public class Input
+    {
+        public static async Task<string[]> GetInput(string filename)
+        {
+            using var sr = new StreamReader(filename);
+            var content = await sr.ReadToEndAsync();
+            return content.Split(Environment.NewLine);
+        }
+    }
+}
---
+++ 3/BinaryDiagnostics/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BinaryDiagnostics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
